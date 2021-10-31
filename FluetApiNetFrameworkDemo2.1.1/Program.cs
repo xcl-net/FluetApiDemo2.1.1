a43428b3c8188afb5a04945223a653b0706582f4
xiaochuanling <461330197@qq.com>
2021-10-31 15:10:54 +0800

    2.5.6 自定义约定;

--- FluetApiNetFrameworkDemo2.1.1/Program.cs
+++ FluetApiNetFrameworkDemo2.1.1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,7 +12,8 @@ namespace FluetApiNetFrameworkDemo2._1._1
 {
     /// <summary>
     /// 2.2 约定
-    /// 2.2.4 复杂类型约定
+    /// 2.2.5 
+    /// 6. 自定义特性
     /// </summary>
     public class Program
     {
@@ -24,6 +26,7 @@ namespace FluetApiNetFrameworkDemo2._1._1
                      new Order
                      {
                          Name = "xcllxc",
+                         BookName = "english"
                     
                      });
 
@@ -51,38 +54,66 @@ namespace FluetApiNetFrameworkDemo2._1._1
 
         public DbSet<Order> Order { get; set; }
 
+
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
 
-            modelBuilder.Types()
-                .Configure(c => c.ToTable(GetTableName(c.ClrType)));
+            modelBuilder.Properties()
+                .Where(x => x.GetCustomAttributes(false).OfType<NonUnicode>().Any())
+                .Configure(c => c.IsUnicode(false));
 
+            //modelBuilder.Properties()
+            //    .Where(x=>x.Name == "BookName")
+            //    .
+
+            //modelBuilder.Properties()
+            //    .Where(x => 
+            //    x.GetCustomAttributes(false)
+            //    .OfType<IsUnicode>().Any())
+            //    .Configure(c =>
+            //    c.IsUnicode(c.ClrPropertyInfo.GetCustomAttributes<IsUnicode>));
+
+
+            modelBuilder.Properties()
+                .Having(x =>
+                x.GetCustomAttributes(false).OfType<IsUnicode>().FirstOrDefault())
+                .Configure((config, att) => config.IsUnicode(att.Unicode));
+            //Having 起到过滤的作用;
 
             base.OnModelCreating(modelBuilder);
 
         }
-
-        //给表名字统一加后缀,前缀
-        private string GetTableName(Type type)
-        {
-            //var result = Regex.Replace(type.Name, ".[A-Z]",
-            //    m => m.Value[0] + "_" + m.Value[1]);
-
-
-            var result = Regex.Replace(type.Name, "[A-Z]+","nn__nn");
-
-            return result.ToString();
-        }
     }
 
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]public class NonUnicode : Attribute
+    { 
+   
+    }
+
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class IsUnicode : Attribute
+    {
+        public bool Unicode { get; set; }
+        public IsUnicode(bool isUnicode)
+        {
+            Unicode = isUnicode;
+        }
+    }
 
 
     public class Order
     {
 
         public int Id { get; set; }
+        
+        [NonUnicode]
         public string Name { get; set; }
 
+        public string City { get; set; }
+
+        public string BookName { get; set; }
     }
 
 
