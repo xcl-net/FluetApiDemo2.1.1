73e15a9ada569ef2476b4e557435ec0dde7c8f04
xiaochuanling <461330197@qq.com>
2021-10-31 10:00:48 +0800

    Id结尾的默认生成为主键自动递增;多个Id结尾的属性,必须显示添加一个字段为主键;

--- FluetApiNetFrameworkDemo2.1.1/Program.cs
+++ FluetApiNetFrameworkDemo2.1.1/Program.cs
@@ -7,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace FluetApiNetFrameworkDemo2._1._1
 {
-
+    /// <summary>
+    /// 2.2 约定
+    /// 2.2.1 类型发现
+    /// </summary>
     public class Program
     {
         static void Main(string[] args)
@@ -15,18 +18,17 @@ namespace FluetApiNetFrameworkDemo2._1._1
 
             using (var efDb = new EfDbContext())
             {
-                efDb.Blog.Add(
-                    new Blog
+                efDb.Department.Add(
+                    new Department
                     {
-                        Name = "xcll",
-                        Url = "www.baidu.com"
+                        Name = "xcll"
                     }
                     );
                 efDb.SaveChanges();
 
 
 
-                var name = efDb.Blog.First().Name;
+                var name = efDb.Department.First().Name;
                 Console.WriteLine(name); //运行可以直接打印出来 "xcll"
             }
 
@@ -39,26 +41,41 @@ namespace FluetApiNetFrameworkDemo2._1._1
 
     public class EfDbContext : DbContext
     {
-        public EfDbContext():base("name=ConnectionString")
+        public EfDbContext() : base("name=ConnectionString")
         {
-            //禁用数据库初始化策略
-            //Database.SetInitializer<EfDbContext>(null); //打开注释,ef则不会自动迁移数据库
-
-
-            #region 方案一: 构造函数里,指定EF数据库生成策略;
-            ////如果数据库不存在, 就创建
-            //Database.SetInitializer(new CreateDatabaseIfNotExists<EfDbContext>());
-
-            ////总是创建数据库, 无论存在与否
-            //Database.SetInitializer(new DropCreateDatabaseAlways<EfDbContext>());
-
-            ////如果EF检测到数据库模型发生了变化, 将更新模型
-            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EfDbContext>()); 
-            #endregion
-
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EfDbContext>());
         }
 
-        public DbSet<Blog> Blog { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<StudentAddress> StudentAddress {  get;}
     }
 
+
+
+    public class Department
+    {
+        //primary key
+        public int DepartmentId { get; set; } //Id结尾的默认生成为主键自动递增;
+        public string Name { get; set; }
+
+
+        ////Navigation property
+        //public virtual ICollection<Course> Courses { get; set; }
+
+    }
+
+    public class StudentAddress
+    {
+        /// <summary>
+        /// 必须 显示添加一个字段为主键
+        /// </summary>
+        public int Id { get; set; }
+        public int AddressId { get; set; }
+        public int StudentId { get; set; }
+    }
+
+    //public class Course
+    //{
+    //    public int MyProperty { get; set; }
+    //}
 }
